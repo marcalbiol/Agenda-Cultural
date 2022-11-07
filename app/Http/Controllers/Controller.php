7cb4b73bd9816a6bbb8233e6961dd12f815a72cf
marcalbiol <marcalbiol48@gmail.com>
2022-11-07 21:20:22 +0100

    Add creado modelo Eventos y ruta para importar data del json

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,16 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+
+    public static function class()
+    {
+        $file = file_get_contents('https://analisi.transparenciacatalunya.cat/resource/rhpv-yr4f.json');
+
+        $data = json_decode($file, false);
+
+        var_dump($data);
+
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/importData', function () {
+    return \App\Http\Controllers\Controller::class();
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('hola', function() { return "Hola món!"; });
