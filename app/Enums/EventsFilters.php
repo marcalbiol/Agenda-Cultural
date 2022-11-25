f7686afc7e765296e3d3a247b4460b6544c84be7
marcalbiol <marcalbiol48@gmail.com>
2022-11-25 20:00:19 +0100

    Creado enum para las diferentes columnas de la bd

---
+++ app/Enums/EventsFilters.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+
+enum EventsFilters: string
+{
+
+    case DENOMINACI = 'denominaci';
+    case CODI = 'codi';
+    case COMARCA_I_MUNICIPI = "comarca_i_municipi";
+    case TAGS_CATEGO_ES = "tags_categor_es";
+
+}
+
