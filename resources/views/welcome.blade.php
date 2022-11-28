9daba578902dad32a2d858680233f9c213758bab
Marc Lopez <marclopezmarco469@gmail.com>
2022-11-28 22:54:37 +0100

    css hardcode, pendiente de arreglar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,6 @@
 
     <title>Eventos</title>
 
-    @vite(['resources/css/app.css','resources/css/template/calendar.css','resources/js/template/calendar.js']);
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
@@ -18,6 +17,9 @@
 
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.4/dist/flowbite.min.css" />
     <link rel="stylesheet" href="https://unpkg.com/flowbite@1.5.4/dist/flowbite.min.css" />
+
+    @vite(['resources/css/app.css','resources/css/template/calendar.css','resources/js/template/calendar.js']);
+
 
 </head>
 <body>
