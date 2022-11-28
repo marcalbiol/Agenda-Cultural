cd2c12a648e7d9901c2aca66e9d6059648512334
Marc Lopez <marclopezmarco469@gmail.com>
2022-11-28 21:07:11 +0100

    implements events views index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Eventos</title>
 
     @vite(['resources/css/app.css','resources/css/template/calendar.css','resources/js/template/calendar.js']);
 
