d10ae137789842bb671b281b70e1b9ba68d40707
marcalbiol <marcalbiol48@gmail.com>
2023-01-11 00:05:01 +0100

    Add footer

---
+++ resources/views/components/template/footer.blade.php
@@ -0,0 +1,28 @@
+<footer class="pt-4 my-md-5 pt-md-5 border-top">
+    <div class="row" style="margin-left: 16rem; margin-top: 3rem">
+        <div class="col-12 col-md">
+            <img class="mb-2" src="{{ asset('assets/img/logo_gene.png') }}" alt="" width="150px" height="150px">
+            <span style="color: black; !important;">Departament de cultura</span>
+            <small class="d-block mb-3 text-muted">&copy; 2022-2013</small>
+        </div>
+        <div class="col-6 col-md">
+            <span style="color: black; font-size: 30px; !important;">Notícias</span>
+            <ul class="list-unstyled text-small">
+                <li><a class="text-muted" href="http://www.quefer.cat/events/234">Éxito del Circo Raluy</a></li>
+                <li><a class="text-muted" href="http://www.quefer.cat/events/149">Teatre Jove de Tarragona</a></li>
+                <li><a class="text-muted" href="http://www.quefer.cat/events/8">Fira de Sant Ermengol</a></li>
+                <li><a class="text-muted" href="http://www.quefer.cat/events/424">Payasos</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-md">
+            <span style="color: black; font-size: 30px; !important;">Conecta</span>
+            <ul class="list-unstyled">
+                <li><a class="text-muted" href="#">Facebook</a></li>
+                <li><a class="text-muted" href="#">Instagram</a></li>
+                <li><a class="text-muted" href="#">Twitter</a></li>
+                <li><a class="text-muted" href="#">LinkedIn</a></li>
+            </ul>
+        </div>
+    </div>
+</footer>
+</div>
