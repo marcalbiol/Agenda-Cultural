53097ca53db6914245dab478c15e2b01ed8d33a4
marcalbiol <marcalbiol48@gmail.com>
2023-01-11 00:10:45 +0100

    Fix show view Vite

--- resources/views/components/template/navuser.blade.php
+++ resources/views/components/template/navuser.blade.php
@@ -1,27 +1,20 @@
 <nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-    <div class="container flex-wrap items-center mx-auto">
+    <div class="container flex-nowrap items-center mx-auto">
         <a href="http://quefer.cat/" class="flex items-center" style="float: left">
             <img src="{{ asset('assets/img/logo.jpeg') }}" class="h-6 mr-3 sm:h-9" alt="QueFer Logo" style="width: 170px; height: 70px">
-            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">QueFer</span>
+            <span class="self-center text-xl font-bold whitespace-nowrap dark:text-white" style="color: black !important;">QueFer</span>
         </a>
-        
+
         <div style="float: right; margin-top: 10px; margin-right: 50px">
+            <a href="http://twitter.com">
+                <img src="{{ asset('assets/img/twitter.png') }}" alt="Twitter" style="height: 50px; float: right">
+            </a>
             <a href="http://facebook.com">
                 <img src="{{ asset('assets/img/facebook.png') }}" alt="Facebook" style="height: 50px; float: right">
             </a>
             <a href="http://instagram.com">
                 <img src="{{ asset('assets/img/instagram.png') }}" alt="Instagram" style="height: 50px; float: right">
             </a>
-            <a href="http://twitter.com">
-                <img src="{{ asset('assets/img/twitter.png') }}" alt="Twitter" style="height: 50px; float: right">
-            </a>
-        </div>
-        
-        <div class="flex md:order-2">
-            <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
-                <span class="sr-only">Open main menu</span>
-                <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-            </button>
         </div>
     </div>
 </nav>
